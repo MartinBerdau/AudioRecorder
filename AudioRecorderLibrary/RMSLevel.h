bdc95d95e9bd96eba81705bbacafde0ed4096a1b
MartinBerdau <49233084+MartinBerdau@users.noreply.github.com>
2020-07-13 20:47:35 +0200

    Kommentar + Lizenz

--- AudioRecorderLibrary/RMSLevel.h
+++ AudioRecorderLibrary/RMSLevel.h
@@ -1,6 +1,6 @@
 /* 
 
-Copyright 2020 <Name>
+Copyright 2020 Martin Berdau
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,24 +19,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-<kurze Erklärung, auf Englisch>
+This file computes a RMS value using exponential smoothing on past peak values.
 
-To Do
---> Links zu Quellen ggf.
---> nicht selbsterklärende Stellen im Code kommentieren
-
-Idee:   analyze_rms nutzen, um einen RMS-Wert als Faktor zu erhalten
-        und dann mit dieser Klasse einen geglätteten RMS-Wert in dbFS
-        berechnen. Dazu Glättungszeit Tau in Sekunden angeben
-        (typisch: slow->1s, fast->0.125s).
-Prinzip:Glättung mit TP-Filter 1. Ordnung mit folgenden Koeffizienten:
-        b = [1-alpha], a = [1, -alpha],
-        mit alpha = exp(-2/(Tau * SampleFrequenz))
- 
-        in Differenzen-Gleichung-Form:
-            a1 * y(k) + a2 * y(k-1)  =  b1 * x(k)
-        ->  y(k) - alpha * y(k-1) = (1-alpha) * x(k)
-        ->  y(k) = (1-alpha) * x(k) + alpha * y(k-1)
  */
 
 #pragma once
